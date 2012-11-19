549934115bb9f71e62cb2b7ef6ed6c712615c9f6
Jean Caillé <jcaille@gmail.com>
2012-11-19 19:20:14 +0100

    Added method and test for the intersection of two lines
    !!! Some method returns logical but problematics results !!!
    For the intersection of two parallel segment sharing only one
    extremity, the method returns Null (as expected), but this might be a
    problem around Saddle Point.

--- test/WindowTest.java
+++ test/WindowTest.java
@@ -13,7 +13,7 @@ public class WindowTest {
 	public void testThirdVertex() {
 		Random picker = new Random(12245);
 		Window myWindow = new Window();
-		for(int i=0 ; i<10000 ; i++){
+		for(int i=0 ; i<10 ; i++){
 			Point_2 expectedResult = new Point_2((picker.nextDouble()*10)-5, (picker.nextDouble()*10)-5);
 			Point_2 secondVertex = new Point_2(picker.nextDouble()*5, 0);
 			double b0 = 0 ;
@@ -23,8 +23,49 @@ public class WindowTest {
 			int tau = (int) Math.signum(expectedResult.y) ;
 			assertEquals("Y coordinates are not the same", expectedResult.y, myWindow.getThirdTriangleVertex(b0, b1, d0, d1, tau).y, 12);
 			assertEquals("X Coordinates are not the same", expectedResult.x, myWindow.getThirdTriangleVertex(b0, b1, d0, d1, tau).x, 12);
-			System.out.println(expectedResult.y +" "+ myWindow.getThirdTriangleVertex(b0, b1, d0, d1, tau).y);
-		}		
+			//System.out.println(expectedResult.y +" "+ myWindow.getThirdTriangleVertex(b0, b1, d0, d1, tau).y);
+		}
 	}
+	
+	@Test
+	public void testIntersectionOfLines() {
+		Random picker = new Random(12345) ;
+		Window myWindow = new Window() ;
+		
+		//test with two zero-length lines ;
+		Point_2  a = new Point_2(0,0);
+		assertNull(myWindow.intersectLines(a, a, a, a));
+		
+		//test with a zero-length line ;
+		Point_2 b = new Point_2(1,1) ;
+		Point_2 c = new Point_2(0,1) ;
+		assertNull(myWindow.intersectLines(a, a, b, c));
+		
+		//test with two identical lines ;
+		assertNull(myWindow.intersectLines(a, b, a, b));
+		
+		//test with two lines that do not intersect at all;
+		Point_2 d = new Point_2(1,2) ;
+		assertNull(myWindow.intersectLines(a, b, c, d));
+		
+		//test with two lines that do cross, but outside the segment
+		Point_2 e = new Point_2(0,2) ;
+		assertNull(myWindow.intersectLines(a, b, c, e)) ;
+		
+		//test with lines that intersect at one end of the segment
+		assertEquals(a, myWindow.intersectLines(a, b, a, c)) ;
+		
+		//final test
+		Point_2 f = new Point_2(2,2) ;
+		Point_2 g = new Point_2(2,0) ;
+		assertEquals(b, myWindow.intersectLines(a, f, e, g));
+		
+		//test reversability
+		assertEquals(myWindow.intersectLines(a, f, e, g), myWindow.intersectLines(e, g, a, f));
+		assertEquals(myWindow.intersectLines(a, f, e, g), myWindow.intersectLines(f, a, e, g)) ;
 
+		//test if two lines are parallel but intersecting at only one point
+		assertNull(myWindow.intersectLines(a, b, b, f));
+	}
 }
+
